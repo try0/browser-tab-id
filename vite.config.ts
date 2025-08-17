a2203965cf1be607a60525be0336f53c55130cc5
Ryo <try0.development@gmail.com>
2025-08-17 21:58:07 +0900

    LICENSE

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,23 @@
 import { defineConfig } from 'vite'
 import dts from 'vite-plugin-dts';
+import banner from 'vite-plugin-banner';
+import pkg from './package.json' assert { type: 'json' };
+
 const isLib = process.env.BUILD_LIB === 'true';
+const licenseBanner = `
+/*!
+ * @try0/browser-tab-id v${pkg.version}
+ * (c) 2025 try0 MIT License.
+ */
+`.trim();
 
 export default defineConfig({
     plugins: [
         dts({
             outDir: 'dist',
             rollupTypes: true,
-        })
+        }),
+        banner(licenseBanner), 
     ],
     base: "./",
     publicDir: isLib ? false : 'public',
